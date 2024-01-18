ac566cb6ffa0bd9a1a2c5d424e7876c3ba85e259
Raghadkatout <ragahdkatout67@gmail.com>
2024-01-18 12:42:09 +0300

    newtodojs

--- js/app.js
+++ js/app.js
@@ -3,27 +3,35 @@
 let UserName = prompt("Please Enter your Name:");
 console.log(UserName);
 
-let Usergender = prompt("Enter your gender (Female or male)");
-console.log(Usergender);
 
+while (true) {
+    var Usergender = prompt("Enter your gender (Female or male)");
+
+    if (Usergender === "male" || Usergender === "Male" || Usergender === "MALE" || Usergender === "female" || Usergender === "Female" || Usergender === "FEMALE") {
+        alert("Your Gender is " + Usergender);
+        break;
+    }
+    else {
+        alert("you Enterd :" + Usergender + ", You should Enter (Female or male) , Please try again");
+    }
+
+}
 let ageInt;
 
-while (true) 
-{
+while (true) {
     let UserAge = prompt("Please Enter your age:")
     ageInt = parseInt(UserAge);
 
-    if(ageInt>0)
-    {
+    if (ageInt > 0) {
         break;
     }
-    else
-    {
+    else {
         alert("you Enterd :" + UserAge + ", this number less than 1, Please try again");
     }
-    
+
 }
 
+console.log(ageInt);
 
 alert("Your age is " + ageInt);
 
